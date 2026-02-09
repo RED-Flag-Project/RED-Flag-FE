a4eea605a6cd148817e03a547b902b2a480d37ce
Park Seeun <155715979+JMUNG1@users.noreply.github.com>
2026-02-10 00:10:54 +0900

    Feat/#6/이미지 업로드 헤더 구현

---
+++ src/components/upload/UploadHeader.jsx
@@ -0,0 +1,12 @@
+export default function UploadHeader() {
+  return (
+    <div className="upload-header">
+      <h1>증거 이미지 업로드</h1>
+      <p>
+        AI가 피싱·스캠 위험도를 자동으로 판단해 드려요
+        <br />
+        의심스러운 채팅이나 문자 화면을 업로드하세요
+      </p>
+    </div>
+  );
+}
