f35ce4e7b0c596fcaa72e477ae15eca47681eabd
Jihyun <hjh20030615@gmail.com>
2026-02-10 07:34:53 +0900

    Feat/#8/보안정보 메인 페이지 UI 제작 완료

---
+++ src/pages/security/NewsDetail.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NewsDetail = () => {
+    return (
+        <div className='NewsDetail_wrap'>
+            전체 보안 뉴스 페이지입니다.
+        </div>
+    )
+}
+
+export default NewsDetail
